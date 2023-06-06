b0cd702f1a0a85bb97e726e79ff902620acbdf83
oppalaa <oppala7@gmail.com>
2023-06-06 11:09:39 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,7 @@
+from django.shortcuts import render
+
+# Create your views here.
+def raavan(request):
+    return render(request,'raavan.html')
+def shiva(request):
+    return render(request,'shiva.html')
